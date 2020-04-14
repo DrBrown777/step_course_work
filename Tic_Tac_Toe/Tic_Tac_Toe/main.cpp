67b56b79a052a2efd0a11ba6bc6849ca6944866b
Роман Терзиев <roman_terziev@hotmail.com>
2020-04-14 13:13:24 +0300

    v.1.09-7

--- Tic_Tac_Toe/Tic_Tac_Toe/main.cpp
+++ Tic_Tac_Toe/Tic_Tac_Toe/main.cpp
@@ -14,8 +14,7 @@ const int init_weight = 100; /*Начальный вес матрицы весо
 const int precision_coef = 50; /*Точность генератора хода для Smart игрока*/
 const double step_coef = 0.65; /*Коэфициент обучения*/
 const int step_learn = 20; /*Шаг обучения*/
-const int number_of_games = 10; /*Кол-во игр которые должен сыграть Smart игрок
-								для обучения*/
+const int number_of_games = 10; /*Кол-во игр которые должен сыграть Smart игрок для обучения*/
 
 bool game_over, wins; /*Флаговые: конец игры, победа*/
 int x_wins = 0, o_wins = 0, d_wins = 0; /*накопительные переменные для статы*/
@@ -30,23 +29,20 @@ struct DataBase /*База знаний smart игрока*/
 };
 
 void welcome(); /*Отображает приветствие*/
-void start_game(); /*Отображает стартовое меню игры и определяет последовательность
-				   вызова функций в зависимоти от типа игры*/
+void start_game(DataBase*, int*); /*Отображает стартовое меню игры и определяет последовательность вызова функций в зависимоти от типа игры*/
 void setup(int*); /*Функция инициализации флаговых переменных*/
-void type_symbol(bool*, char*, char*, int*); /*Функция рандомно определяет кто будет
-											 играть за X а кто за O*/
+void type_symbol(bool*, char*, char*, int*); /*Функция рандомно определяет кто будет играть за X а кто за O*/
 void clear_field(); /*Функция очищает игровые поля*/
-void draw_field(); /*Функция выводит игровое поле*/
+void display_field(); /*Функция выводит игровое поле*/
 int random_player(); /*Функция возвращает возможный ход, случайно, в стратегии Random*/
-int input_events(bool*, int*); /*Функция возвращает ход сделанный пользователем с клавиатуры,
-							   и выводит оставшиеся варианты хода*/
-char check_wins(int*); /*Функция проверяет на победу после каждого хода*/
+int input_events(bool*, int*, DataBase*, int*); /*Функция возвращает ход сделанный пользователем с клавиатуры, и выводит оставшиеся варианты хода*/
+char check_wins(int*, int*); /*Функция проверяет на победу после каждого хода*/
 void wins_stat(char, int*); /*Функция выводит поздравление о выиграше*/
 void game_logic(int, int*, bool*, int*, char*, char*); /*Функция логики игры*/
-void play_game(int*, bool*, int*, char*, char*); /*Функция loop цикла 1 партии*/
+void play_game(int*, DataBase*, int*); /*Функция loop цикла 1 партии*/
 
-DataBase* push_database(DataBase*, int&);
-int get_situation(DataBase*, int);
+DataBase* push_database(DataBase*, int*);
+int get_situation(DataBase*, int*);
 int get_smart_random(int*);
 void smart_learn(); /*Рекурсивная функция обучения*/
 
@@ -54,8 +50,12 @@ int main()
 {
 	setlocale(0, "");
 	srand(unsigned(time(0)));
+
+	int size_database = 0;
+	DataBase* Collections = new DataBase[size_database];
+
 	welcome();
-	start_game();
+	start_game(Collections, &size_database);
 	return 0;
 }
 
@@ -64,14 +64,11 @@ void welcome()
 	cout << "\t" << "*Добро пожаловать в Tic-Tac-Toe!*" << "\n\n";
 }
 
-void start_game()
+void start_game(DataBase* Collections, int* size_database)
 {
 	int menu;
 	bool loop = true;
-	int draw; /*Проверка на ничью*/
-	bool turn; /*Очередь хода*/
 	int type_game; /*тип игры, Random или Smart*/
-	char symbol_player_1, symbol_player_2; /*символьные переменные кто за что играет*/
 
 	while (loop == true)
 	{
@@ -88,30 +85,21 @@ void start_game()
 		{
 		case 1:
 			type_game = 1;
-			setup(&draw);
-			type_symbol(&turn, &symbol_player_1, &symbol_player_2, &type_game);
-			clear_field();
-			play_game(&draw, &turn, &type_game, &symbol_player_1, &symbol_player_2);
+			play_game(&type_game, Collections, size_database);
 			break;
 		case 2:
 		{
 			type_game = 2;
-			setup(&draw);
-			type_symbol(&turn, &symbol_player_1, &symbol_player_2, &type_game);
-			clear_field();
-			play_game(&draw, &turn, &type_game, &symbol_player_1, &symbol_player_2);
+			play_game(&type_game, Collections, size_database);
 			break;
 		}
 		case 3:
 			type_game = 3;
 			for (int i = 0; i < number_of_games; i++)
 			{
-				setup(&draw);
-				type_symbol(&turn, &symbol_player_1, &symbol_player_2, &type_game);
-				clear_field();
-				play_game(&draw, &turn, &type_game, &symbol_player_1, &symbol_player_2);
+				play_game(&type_game, Collections, size_database);
 			}
-			cout << "Победы X: " << x_wins << " Победы O: " << o_wins << " Ничьи: " << d_wins << "\n\n";
+			cout << "Игрок Smart теперь очень умный!" << "\n\n";
 			break;
 		default:
 			break;
@@ -121,25 +109,130 @@ void start_game()
 	}
 }
 
+void play_game(int* type_game, DataBase* Collections, int* size_database)
+{
+	int move; /*Переменная содержит ход*/
+	bool turn; /*Очередь хода*/
+	char player_1, player_2; /*символьные переменные кто за что играет*/
+	int draw; /*Проверка на ничью*/
+
+	setup(&draw);
+	type_symbol(&turn, &player_1, &player_2, type_game);
+
+	while (game_over != true)
+	{
+		if (*type_game == 1 || *type_game == 2)
+			display_field();
+		move = input_events(&turn, type_game, Collections, size_database);
+		draw++;
+		game_logic(move, &draw, &turn, type_game, &player_1, &player_2);
+	}
+}
+
+int input_events(bool* turn, int* type_game, DataBase* Collections, int* size_database)
+{
+	int move;
+	if ((*turn && *type_game == 1) || (*turn && *type_game == 2))
+	{
+		cout << "Варианты хода:" << "\n\n";
+		for (int i = 0; i < field_size; i++)
+		{
+			if (i == 2 || i == 5 || i == 8)
+				cout << "\t" << setw(2) << "-" << FieldVar[i] << "-" << endl;
+			else
+				cout << "\t" << setw(2) << "-" << FieldVar[i] << "-  |";
+		}
+		cout << endl;
+		do
+		{
+			cout << "Сделайте ход (1-9) -> ";
+			cin >> move;
+		} while (move < 1 || move > 9 || Field[move - 1] == 'X' || Field[move - 1] == 'O');
+	}
+	else if ((*turn && *type_game == 3) || *type_game == 2)
+	{
+		if (get_situation(Collections, size_database) != 0)
+			Collections = push_database(Collections, size_database);
+		
+		int index = get_situation(Collections, size_database);
+
+		return get_smart_random(Collections[index].MyWeight);
+	}
+	else
+	{
+		return random_player();
+	}
+	return move;
+}
+
+void game_logic(int move, int* draw, bool* turn, int* type_game, char* player_1, char* player_2)
+{
+	char XOD;
+	if ((*turn && *type_game == 1) || (*turn && *type_game == 2))
+	{
+		Field[move - 1] = *player_1;
+		FieldVar[move - 1] = '-';
+		XOD = check_wins(draw, type_game);
+		if (wins)
+		{
+			wins_stat(XOD, type_game);
+			return;
+		}
+		*turn = false;
+	}
+	else if ((*turn && *type_game == 3) || *type_game == 2)
+	{
+		if (*type_game == 2)
+		{
+			Field[move] = *player_2;
+			FieldVar[move] = '-';
+		}
+		else
+		{
+			Field[move] = *player_1;
+		}
+		XOD = check_wins(draw, type_game);
+		if (wins)
+		{
+			wins_stat(XOD, type_game);
+			return;
+		}
+		*type_game == 2 ? *turn = true : *turn = false;
+	}
+	else
+	{
+		Field[move] = *player_2;
+		if (*type_game == 1 || *type_game == 2) FieldVar[move] = '-';
+		XOD = check_wins(draw, type_game);
+		if (wins)
+		{
+			wins_stat(XOD, type_game);
+			return;
+		}
+		*turn = true;
+	}
+}
+
 void setup(int* draw)
 {	
 	game_over = false;
 	wins = false;
 	*draw = 0;
+	clear_field();
 }
 
-void type_symbol(bool* turn, char* symbol_player_1, char* symbol_player_2, int* type_game)
+void type_symbol(bool* turn, char* player_1, char* player_2, int* type_game)
 {
 	int type_symbol = rand() % 2 + 1;
 
 	if (type_symbol == 2)
 	{
-		*symbol_player_1 = 'O'; *symbol_player_2 = 'X';
+		*player_1 = 'O'; *player_2 = 'X';
 		*turn = false;
 	}
 	else
 	{
-		*symbol_player_1 = 'X'; *symbol_player_2 = 'O';
+		*player_1 = 'X'; *player_2 = 'O';
 		*turn = true;
 	}
 
@@ -168,7 +261,7 @@ void clear_field()
 	}
 }
 
-void draw_field()
+void display_field()
 {
 	system("cls");
 	cout << "\n\n";
@@ -209,38 +302,7 @@ int random_player()
 	return move;
 }
 
-int input_events(bool* turn, int* type_game)
-{
-	int move;
-	if ((*turn && *type_game == 1) || (*turn && *type_game == 2))
-	{
-		cout << "Варианты хода:" << "\n\n";
-		for (int i = 0; i < field_size; i++)
-		{
-			if (i == 2 || i == 5 || i == 8)
-				cout << "\t" << setw(2) << "-" << FieldVar[i] << "-" << endl;
-			else
-				cout << "\t" << setw(2) << "-" << FieldVar[i] << "-  |";
-		}
-		cout << endl;
-		do
-		{
-			cout << "Сделайте ход (1-9) -> ";
-			cin >> move;
-		} while (move < 1 || move > 9 || Field[move - 1] == 'X' || Field[move - 1] == 'O');
-	}
-	else if ((*turn && *type_game == 3) || *type_game == 2)
-	{
-		return random_player();
-	}
-	else
-	{
-		return random_player(); /*smart игрок дописать вызов*/
-	}
-	return move;
-}
-
-char check_wins(int* draw)
+char check_wins(int* draw, int* type_game)
 {
 	int victory[8][3] = { {0, 1 , 2}, {3, 4, 5}, {6, 7, 8},
 	{0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6} };
@@ -252,14 +314,14 @@ char check_wins(int* draw)
 			Field[victory[i][0]] != ' ')
 		{
 			wins = true;
-			Field[victory[i][0]] == 'X' ? x_wins++ : o_wins++;
+			if (*type_game != 3) Field[victory[i][0]] == 'X' ? x_wins++ : o_wins++;
 			return Field[victory[i][0]] == 'X' ? 'X' : 'O';
 		}
 	}
 	if (*draw == 9)
 	{
 		wins = true;
-		d_wins++;
+		if (*type_game != 3) d_wins++;
 		return 'D';
 	}
 	return 'U';
@@ -269,7 +331,7 @@ void wins_stat(char XOD, int* type_game)
 {
 	if (*type_game == 1 || *type_game == 2)
 	{
-		draw_field();
+		display_field();
 		if (XOD != 'D')
 			cout << "\t" << "Поздравляем, победили - " << XOD << " !\n\n";
 		else
@@ -280,98 +342,36 @@ void wins_stat(char XOD, int* type_game)
 	game_over = true;
 }
 
-void game_logic(int move, int* draw, bool* turn, int* type_game, char* symbol_player_1, char* symbol_player_2)
+DataBase* push_database(DataBase* Collect, int* size)
 {
-	char XOD;
-	if ((*turn && *type_game == 1) || (*turn && *type_game == 2))
-	{
-		Field[move - 1] = *symbol_player_1;
-		FieldVar[move - 1] = '-';
-		XOD = check_wins(draw);
-		if (wins)
-		{
-			wins_stat(XOD, type_game);
-			return;
-		}
-		*turn = false;
-	}
-	else if ((*turn && *type_game == 3) || *type_game == 2)
-	{
-		if (*type_game == 2)
-		{
-			Field[move] = *symbol_player_2;
-			FieldVar[move] = '-';
-		}
-		else
-		{
-			Field[move] = *symbol_player_1;
-		}
-		XOD = check_wins(draw);
-		if (wins)
-		{
-			wins_stat(XOD, type_game);
-			return;
-		}
-		*type_game == 2 ? *turn = true : *turn = false;
-	}
-	else
-	{
-		Field[move] = *symbol_player_2;
-		if (*type_game == 1 || *type_game == 2) FieldVar[move] = '-';
-		XOD = check_wins(draw);
-		if (wins)
-		{
-			wins_stat(XOD, type_game);
-			return;
-		}
-		*turn = true;
-	}
-}
+	DataBase* Temp = new DataBase[*size + 1];
 
-void play_game(int* draw, bool* turn, int* type_game, char* symbol_player_1, char* symbol_player_2)
-{
-	int move;
-
-	while (game_over != true)
-	{
-		if (*type_game == 1 || *type_game == 2)
-			draw_field();
-		move = input_events(turn, type_game);
-		(*draw)++;
-		game_logic(move, draw, turn, type_game, symbol_player_1, symbol_player_2);
-	}
-}
-
-DataBase* push_database(DataBase* Collect, int& size)
-{
-	DataBase* Temp = new DataBase[size + 1];
-
-	for (int i = 0; i < size; i++)
+	for (int i = 0; i < *size; i++)
 	{
 		Temp[i] = Collect[i];
 	}
 
-	strcpy_s(Temp[size].MyField, Field);
+	strcpy_s(Temp[*size].MyField, Field);
 
 	for (int i = 0; i < 9; i++)
 	{
-		if (strncmp(&Temp[size].MyField[i], " ", 1) != 0)
+		if (strncmp(&Temp[*size].MyField[i], " ", 1) != 0)
 		{
-			Temp[size].MyWeight[i] = 0;
+			Temp[*size].MyWeight[i] = 0;
 		}
 	}
 
 	delete[] Collect;
 
-	size++;
+	(*size)++;
 
 	return Temp;
 }
 
-int get_situation(DataBase* Collect, int size)
+int get_situation(DataBase* Collect, int* size)
 {
 
-	for (int i = 0; i < size; i++)
+	for (int i = 0; i < *size; i++)
 	{
 		if (strcmp(Collect[i].MyField, Field) == 0)
 		{
@@ -394,6 +394,8 @@ int get_smart_random(int* mas)
 		summ += mas[i];
 		mas_temp[i] = mas[i];
 	}
+
+	/*Дописать проверку на summ == 0 когда все хода одинаково плохи*/
 
 	for (int i = 0; i < 9; i++)
 	{
